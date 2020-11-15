a6d6cf2e7f45bf03d9f3796142c3690209a33201
Young-oMusician <michal.bitnerowski@outlook.com>
2020-11-15 22:06:06 +0100

    Initial commit

---
+++ src/main/java/com/campaign/banksMarketingCampaign/BanksMarketingCampaignApplication.java
@@ -0,0 +1,13 @@
+package com.campaign.banksMarketingCampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksMarketingCampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksMarketingCampaignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campaign/banksMarketingCampaign/BanksMarketingCampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campaign.banksMarketingCampaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksMarketingCampaignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
